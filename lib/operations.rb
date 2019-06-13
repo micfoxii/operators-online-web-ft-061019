38c410d6116ffdae5d89f2d2d99b4f0735556001
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-13 14:25:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
- if speed > 60 || speed < 40 ? true : false
+ speed > 60 || speed < 40 ? true : false
  end
 end
 
 
 
 def not_safe?(speed)
-	if speed < 40 || speed > 60 ? true : false
+	speed < 40 || speed > 60 ? true : false
 	end
 end
 	
